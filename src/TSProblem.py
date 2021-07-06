86c9c7f258196cf38c60dab91981cec7a96094ba
Xianyu <fanghejin@qq.com>
2021-07-07 00:22:18 +0800

    完成了寻求附近解的函数以及对笔记进行了部分修改

--- src/TSProblem.py
+++ src/TSProblem.py
@@ -7,10 +7,8 @@ which can be solved by tabu-search.
 
 Here is an example.
 """
-# from _typeshed import OpenBinaryMode
-# from os import name
-import numpy as np
 
+import numpy as np
 
 class Error(Exception):
     """Cities and pathes matrix are not conpatible, or matrix are not square matrix."""
@@ -22,6 +20,8 @@ class MapError(Error):
     def __init__(self, message:str) -> None:
         self.message = message
 
+
+INFINITY = 0xFFFFFFFFF
 
 class Map:
     """Acturally, Map is a directed graph, which uses tuple as verteces, ndarray as edges."""
@@ -47,12 +47,39 @@ class Map:
 
 class Tabu_Search:
     """Tabu search class, includes visualization."""
-    def __init__(self, map:np.ndarray) -> None:
-        pass
+    def __init__(self, map:Map) -> None:
+        self.TabuList = list()
+        self.TabuLength = int()
+        self.map = map
+        self.beginCity=self.map.cities[0]
 
 
-# style string
-vertex="\t{name}[shape=circle, label={label}, size=0.1, fillcolor=black, style=filled, fontcolor=white]\n"
+    def neighbours(self, ans:tuple)->tuple:
+        ans_collection=list()
+        ex_collection=list()
+        for i in range(len(self.map.cities)):
+            for j in range(i+1, len(self.map.cities)):
+                if self.map.cities[i]!=self.beginCity and self.map.cities[j]!=self.beginCity:
+                    ex_collection.append((self.map.cities[i], self.map.cities[j]))
+
+        neighbour_collection = list()
+        print(ex_collection)
+        for ex in ex_collection:
+            possible_ans=list(ans)
+            # Exchange 2 elements in ex to generate new answers.
+            x = possible_ans.index(ex[0])
+            y = possible_ans.index(ex[1])
+            possible_ans[x]=ex[1]
+            possible_ans[y]=ex[0]
+
+            if possible_ans not in self.TabuList:
+                neighbour_collection.append(possible_ans)
+
+        return tuple(neighbour_collection)
+
+
+# style template
+vertex="\t{name}[shape=\"circle\", label=\"{label}\", size=0.1, fillcolor=\"black\", style=\"filled\", fontcolor=\"white\"]\n"
 arrow="\t{v1}->{v2}[label=\"{w}\"]\n"
 def visualize_Map(map:Map):
     # Open script file
@@ -70,7 +97,7 @@ def visualize_Map(map:Map):
         for i in range(map.edges().shape[0]):
             for j in range(map.edges().shape[1]):
                 weight = map.edges()[i,j]
-                if weight!=0:
+                if weight!=INFINITY:
                     script.write(arrow.format(v1=verteces[i], v2=verteces[j], w=weight))
 
         script.write("}")
@@ -79,10 +106,11 @@ def visualize_Map(map:Map):
 
 
 def main():
-    mt=np.array([[0,1,0.5,1],[1,0,1,1,],[1.5,5,0,1],[1,1,1,0]])
-    
+    mt=np.array([[INFINITY, 1,1.5,1],[1,INFINITY,1,1,],[1.5,5,INFINITY,1],[1,1,1,INFINITY]])
     map = Map(('A','B','C','D'),mt)
-    visualize_Map(map)
-    print(mt)
+
+    TS=Tabu_Search(map)
+    TS.beginCity='A'
+    print(TS.neighbours(('A','B','C','D')))
 
 main()
